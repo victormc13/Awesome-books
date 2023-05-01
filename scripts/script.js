3f4954762b4630e359c94782a388ba952ab12652
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-05-01 17:04:32 -0400

    Create the Add button

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,46 @@
 let books = [
   {
-    title: title1,
-    author: autor1,
-  }
+    title: 'title1',
+    author: 'autor1',
+  },
+  {
+    title: 'title2',
+    author: 'autor2',
+  },
 ];
 
-function addBook(title, author) {
+function loadHTML(index) {
+  const superHTML = `
+  <article class="book">
+    <h4 id="">${books[index].title}</h4>
+    <p id="">${books[index].author}</p>
+    <button id="remove-button">Remove</button>
+  </article>
+  `;
+
+  document.querySelector('.booklist-container').innerHTML = superHTML;
+}
+
+function loadBooks() {
+  const booksAmount = books.length;
+
+  for (let i = 0; i < booksAmount; i += 1) {
+    loadHTML(i);
+  }
+}
+
+function addBook(bookTitle, bookAuthor) {
   let newBook = {
-    title: title,
-    author: author
+    title: bookTitle,
+    author: bookAuthor,
   };
   books.push(newBook);
+
+  loadBooks();
 };
 
+const addButton = document.getElementById('add-button');
+const newTitle = document.getElementById('new-title');
+const newAuthor = document.getElementById('new-author');
+
+addButton.addEventListener('click', () => addBook(newTitle.value, newAuthor.value));
