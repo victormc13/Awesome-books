08360589316288aff8b50e52cdc625d1712c3bf8
José Fernández-Caminero <fdezcaminero@gmail.com>
2023-05-02 11:12:39 -0400

    Create remove button

--- scripts/script.js
+++ scripts/script.js
@@ -7,38 +7,58 @@ let books = [
     title: 'title2',
     author: 'autor2',
   },
+  {
+    title: 'Dune',
+    author: 'Frank Herbert',
+  },
 ];
 
 function loadHTML(index) {
   const superHTML = `
-  <article class="book">
+  <li class="book">
     <h4 id="">${books[index].title}</h4>
     <p id="">${books[index].author}</p>
-    <button id="remove-button">Remove</button>
-  </article>
+    <button id="remove-button${index}">Remove</button>
+  </li>
   `;
 
-  document.querySelector('.booklist-container').innerHTML = superHTML;
+  document.querySelector('.booklist-container').insertAdjacentHTML("beforeend", superHTML);
+  document.getElementById('remove-button' + index).addEventListener('click', () => removeBook(index));
 }
 
 function loadBooks() {
   const booksAmount = books.length;
+  const emptyHTML = "";
 
+  document.querySelector('.booklist-container').innerHTML = emptyHTML;
   for (let i = 0; i < booksAmount; i += 1) {
     loadHTML(i);
   }
+
+  // localStorage.setItem('books', JSON.stringify(books));
 }
 
 function addBook(bookTitle, bookAuthor) {
-  let newBook = {
-    title: bookTitle,
-    author: bookAuthor,
-  };
-  books.push(newBook);
+  if (bookTitle !== '' && bookAuthor !== '') {
+    let newBook = {
+      title: bookTitle,
+      author: bookAuthor,
+    };
+    books.push(newBook);
 
-  loadBooks();
+    newTitle.value = '';
+    newAuthor.value = '';
+    loadBooks();
+  }
 };
 
+function removeBook(index) {
+  books.splice(index, 1);
+
+  loadBooks();
+}
+
+window.addEventListener('load', loadBooks());
 const addButton = document.getElementById('add-button');
 const newTitle = document.getElementById('new-title');
 const newAuthor = document.getElementById('new-author');
