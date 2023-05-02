0808b8f16d2d155e175559e7ca60373d704ff6eb
victormc13 <viktorsb14@gmail.com>
2023-05-02 17:42:33 -0500

    Add style for the list of books

--- scripts/script.js
+++ scripts/script.js
@@ -49,14 +49,20 @@ const superLibrary = new Library();
 function loadHTML(index) {
   const superHTML = `
   <li class="book">
-    <h4 id="">${superLibrary.books[index].title}</h4>
-    <p id="">${superLibrary.books[index].author}</p>
+    <div class="book-details">
+    <h4 id="">"${superLibrary.books[index].title}"</h4>
+    <p id="">by ${superLibrary.books[index].author}</p>
+    </div>
     <button id="remove-button${index}">Remove</button>
   </li>
   `;
 
-  document.querySelector('.booklist-container').insertAdjacentHTML('beforeend', superHTML);
-  document.getElementById(`remove-button${index}`).addEventListener('click', () => superLibrary.removeBook(index));
+  document
+    .querySelector('.booklist-container')
+    .insertAdjacentHTML('beforeend', superHTML);
+  document
+    .getElementById(`remove-button${index}`)
+    .addEventListener('click', () => superLibrary.removeBook(index));
 }
 
 function loadBooks() {
